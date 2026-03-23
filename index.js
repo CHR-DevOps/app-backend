0e5bc13f3321e017da18d29b0bd525731c7c3594
robertchriss <chriss.ercau@gmail.com>
2026-03-23 23:15:13 +0200

    update health

--- index.js
+++ index.js
@@ -18,9 +18,10 @@ db.connect((err) => {
 
   const app = createApp(db);
   const PORT = process.env.PORT || 5000;
-  app.get("/health", (req, res) => {
-    res.status(200).send("ok");
-  });
+
+  app.get('/health', (req, res) => res.status(200).send('ok'));
+  app.get('/api/health', (req, res) => res.status(200).send('ok'));
+  
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
